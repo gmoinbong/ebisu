88dfd2862e84a99925b6020e9eaa7dff031b2803
gmoinbong <vladosos.scherba@gmail.com>
2023-06-01 12:49:00 +0300

    initial commit

--- server.mjs
+++ server.mjs
@@ -1,19 +1,18 @@
 import express from 'express';
 import { MongoClient } from 'mongodb';
 import cors from 'cors';
+const url = 'mongodb+srv://admin:qwertyuiop@ebisuclaster.75e7got.mongodb.net/';
+
 
 const app = express();
 const port = 5172;
-const url = 'mongodb+srv://admin:qwertyuiop@ebisuclaster.75e7got.mongodb.net/';
 app.use(cors({ origin: 'http://localhost:5173' }));
 
-// Подключение к MongoDB
 MongoClient.connect(url)
   .then((client) => {
     const db = client.db('ebisu');
     const collection = db.collection('Books');
 
-    // Маршрут для получения документа с полем 'title'
     app.get('/api/book', (req, res) => {
       collection.find({}, { projection: { _id: 0, title: 1 } })
         .toArray()
@@ -26,11 +25,11 @@ MongoClient.connect(url)
         });
     });
 
-    // Запуск сервера
     app.listen(port, () => {
       console.log(`Сервер запущен на порту ${port}`);
     });
   })
+
   .catch((error) => {
     console.error('Ошибка подключения к MongoDB:', error);
   });
